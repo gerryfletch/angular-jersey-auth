e8d2b77c4e90e3007377093494f1732311cc711b
gerryfletch <1997gerry@gmail.com>
2018-06-09 17:36:42 +0100

    Made getMessage function in the message service, that checks for user role.

--- webapp/src/app/_services/message.service.ts
+++ webapp/src/app/_services/message.service.ts
@@ -2,16 +2,21 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs/index';
 import {Message} from '../_models/message.model';
 import {HttpClient} from '@angular/common/http';
+import {AuthenticationService} from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthenticationService) { }
 
-  // getMessage(): Observable<Message> {
-  //
-  // }
+  getMessage(): Observable<Message> {
+    if (this.authService.isLoggedIn()) {
+      return this.http.get<Message>('/api/message/private');
+    } else {
+      return this.http.get<Message>('/api/message/public');
+    }
+  }
 
 }
