9b744c5d4623e01d4e6b7b8f8675d0b05127830b
gerryfletch <1997gerry@gmail.com>
2018-06-09 15:56:20 +0100

    Created login component. Makes http requests, shows errors and conditionally shows/hides.

---
+++ webapp/src/app/components/login/login.component.ts
@@ -0,0 +1,45 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from '../../_services/authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  isLogged: boolean;
+
+  username: string;
+  password: string;
+
+  isError: boolean;
+  error: string;
+
+  constructor(private authService: AuthenticationService) {
+  }
+
+  ngOnInit() {
+    this.isLogged = this.authService.isLoggedIn();
+  }
+
+  login() {
+    if (!this.username || !this.password) {
+      return;
+    }
+
+    this.authService.login(this.username, this.password)
+      .subscribe(
+        val => {
+          this.isError = false;
+          this.error = '';
+          this.isLogged = true;
+        },
+        error => {
+          this.isError = true;
+          this.error = error;
+        }
+      );
+  }
+
+}
