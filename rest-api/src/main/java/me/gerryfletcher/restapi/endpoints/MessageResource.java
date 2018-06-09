5fc34baf2a316b26ef78e9a6b7b49f71bc98c4fc
gerryfletch <1997gerry@gmail.com>
2018-06-09 17:34:43 +0100

    Changed message end points to use Response rather than String.

--- rest-api/src/main/java/me/gerryfletcher/restapi/endpoints/MessageResource.java
+++ rest-api/src/main/java/me/gerryfletcher/restapi/endpoints/MessageResource.java
@@ -1,5 +1,7 @@
 package me.gerryfletcher.restapi.endpoints;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import me.gerryfletcher.restapi.authentication.Role;
 
 import javax.annotation.security.PermitAll;
@@ -16,14 +18,20 @@ public class MessageResource {
     private static String publicMessage = "Hello, world.";
     private static String privateMessage = "You're a logged in user!";
 
+    private Gson gson;
+
+    public MessageResource() {
+        this.gson = new Gson();
+    }
+
     /**
      * @return The public message to anyone who wants it.
      */
     @GET
     @PermitAll
     @Path("public")
-    public String getPublicMessage() {
-        return publicMessage;
+    public Response getPublicMessage() {
+        return formMessageResponse(publicMessage);
     }
 
     /**
@@ -32,8 +40,8 @@ public class MessageResource {
     @GET
     @RolesAllowed(Role.USER)
     @Path("private")
-    public String getPrivateMessage() {
-        return privateMessage;
+    public Response getPrivateMessage() {
+        return formMessageResponse(privateMessage);
     }
 
     /**
@@ -54,5 +62,11 @@ public class MessageResource {
 
         publicMessage = message;
         return Response.status(Response.Status.NO_CONTENT).build();
+    }
+
+    private Response formMessageResponse(String message) {
+        JsonObject response = new JsonObject();
+        response.addProperty("message", message);
+        return Response.ok().entity(gson.toJson(response)).build();
     }
 }
