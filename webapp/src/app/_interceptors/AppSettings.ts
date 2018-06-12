c8e55ba3828f4e33eeda1c16d6d606dc3ea0bd6d
gerryfletch <1997gerry@gmail.com>
2018-06-12 22:52:14 +0100

    The interceptors now succesfully refresh requests and check a global whitelist for urls.

---
+++ webapp/src/app/_interceptors/AppSettings.ts
@@ -0,0 +1,3 @@
+export class AppSettings {
+  public static HTTP_WHITELIST = new Set<string>(['/api/auth/refresh']);
+}
--- webapp/src/app/_interceptors/auth.interceptor.ts
+++ webapp/src/app/_interceptors/auth.interceptor.ts
@@ -1,66 +1,78 @@
 import {Injectable} from '@angular/core';
 import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 
-import {EMPTY, Observable, Subject, throwError} from 'rxjs';
+import {Observable, throwError} from 'rxjs';
 import {TokenService} from '../_services/token.service';
 import {AuthenticationService} from '../_services/authentication.service';
-import {catchError, map, mergeMap} from 'rxjs/internal/operators';
+import {catchError, switchMap} from 'rxjs/internal/operators';
+import {AppSettings} from './AppSettings';
 
 /** Pass untouched request through to the next request handler. */
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  private requests$: Observable<HttpEvent<any>>;
-  private requests: Subject<HttpEvent<any>>;
+  private WHITELIST: Set<string>;
 
   constructor(private tokenService: TokenService, private authService: AuthenticationService) {
-    this.requests = new Subject<HttpEvent<any>>();
-    this.requests$ = this.requests.asObservable();
+    this.WHITELIST = AppSettings.HTTP_WHITELIST;
   }
 
+  /**
+   * @returns {HttpRequest<any>} A cloned http request with the authorization appended.
+   */
   createAuthRequest(req: HttpRequest<any>): HttpRequest<any> {
     return req.clone({
       headers: req.headers.set('Authorization', 'Bearer ' + this.tokenService.getAccessToken())
     });
   }
 
+  /**
+   * Intercept different life cycles of a http request.
+   * Authentication is added to all requests unless they belong to the AppSettings whitelist.
+   * If a 401 unauthorized response is returned, the auth service attempts to refresh the token.
+   * On success, the request will be resent and its response returned. On failure, the user will be logged out
+   * and an error returned.
+   * @param {HttpRequest<any>} req The http request to process.
+   * @param {HttpHandler} next The next handler in the interceptor barrel.
+   */
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Attempting request: ');
-    console.log(req);
-    if (req.url === '/api/auth/refresh') {
+    if (this.WHITELIST.has(req.url)) {
       return next.handle(req);
     }
 
-    // Add auth
     const authReq = this.createAuthRequest(req);
 
     return next.handle(authReq).pipe(
       catchError((err: HttpErrorResponse) => {
-        // attempt to refresh
-        if (err.status === 401) {
-          this.authService.refreshTokens()
-            .subscribe(
-              () => {
-                // the tokens have been refreshed, reattempt request.
-                console.log('The tokens have been refreshed. Retrying request: ');
-                console.log(req);
-                next.handle(req).subscribe(
-                  res => {
-                    this.requests.next(res);
-                    this.requests.complete();
-                  }
-                );
-              },
-              (e) => {
-                this.authService.logout();
-                this.requests.next(e);
-                this.requests.complete();
-                // return EMPTY;
-              }
-            );
+        // Check if the response is unauthorized.
+        if (err.status !== 401) {
+          return throwError(err);
         }
 
-        return this.requests;
+        // New tokens are required. Refresh and get the authenticated response or error.
+        return this.refreshAndRetry(req, next);
+      })
+    );
+  }
+
+  /**
+   * Attempt to refresh our tokens. If authentication is successful, repeat the failed request and return
+   * its observable. Otherwise, log the user out and throw the error response.
+   * @param req The original (failed) request to be retried after authentication.
+   * @param next The next http handler in the interceptor barrel.
+   * @returns {Observable<any>} Either the http response or an error.
+   */
+  private refreshAndRetry(req, next): Observable<any> {
+    return this.authService.refreshTokens().pipe(
+      switchMap(() => {
+        return next.handle(this.createAuthRequest(req));
+      }),
+      catchError((e: HttpErrorResponse) => {
+        // The token refresh was unauthorized, so log them out.
+        if (e.status === 401) {
+          this.authService.logout();
+        }
+        return throwError(e);
       })
     );
   }
