cb66caa93d1c01aec319860f205d33785543e6db
gerryfletch <1997gerry@gmail.com>
2018-06-12 22:52:50 +0100

    Changed auth services to use Token model, and updated methods to use RxJs v6 tap function rather than map.

--- webapp/src/app/_services/token.service.ts
+++ webapp/src/app/_services/token.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import {Observable, of} from 'rxjs/index';
+import {Tokens} from '../_models/tokens.model';
 
 @Injectable({
   providedIn: 'root'
@@ -32,9 +33,9 @@ export class TokenService {
     return localStorage.getItem(this.ACCESS_KEY);
   }
 
-  setTokens(refreshToken: string, accessToken: string) {
-    localStorage.setItem(this.REFRESH_KEY, refreshToken);
-    localStorage.setItem(this.ACCESS_KEY, accessToken);
+  setTokens(token: Tokens) {
+    localStorage.setItem(this.REFRESH_KEY, token.refresh_token);
+    localStorage.setItem(this.ACCESS_KEY, token.access_token);
   }
 
 
