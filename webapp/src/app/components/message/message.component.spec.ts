d649a1576fd4e1117a9f5d33ef808f99eb7fb8a6
gerryfletch <1997gerry@gmail.com>
2018-06-20 16:55:24 +0100

    Created a test to make sure the message appears.

--- webapp/src/app/components/message/message.component.spec.ts
+++ webapp/src/app/components/message/message.component.spec.ts
@@ -1,16 +1,29 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { MessageComponent } from './message.component';
+import {MessageComponent} from './message.component';
+import {MessageService} from '../../_services/message.service';
+import {Observable, of} from 'rxjs';
+import {Message} from '../../_models/message.model';
+import {By} from '@angular/platform-browser';
 
-describe('MessageComponent', () => {
+class MessageServiceStub {
+  getMessage(): Observable<Message> {
+    return of({'message': 'message'} as Message);
+  }
+}
+
+fdescribe('MessageComponent', () => {
   let component: MessageComponent;
   let fixture: ComponentFixture<MessageComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MessageComponent ]
+      declarations: [MessageComponent],
+      providers: [
+        {provide: MessageService, useClass: MessageServiceStub}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +35,12 @@ describe('MessageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display a message', () => {
+    const de = fixture.debugElement.query(By.css('h2'));
+    const el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain('message');
+  });
+
 });
