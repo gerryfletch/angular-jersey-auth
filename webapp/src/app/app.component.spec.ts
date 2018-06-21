46dcbc60a786b96be3b7d1ee53bfd79f846bafca
gerryfletch <1997gerry@gmail.com>
2018-06-21 17:54:18 +0100

    Created tests for App Component to check message and login exist.

--- webapp/src/app/app.component.spec.ts
+++ webapp/src/app/app.component.spec.ts
@@ -1,27 +1,53 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {By} from '@angular/platform-browser';
+import {Component, NO_ERRORS_SCHEMA} from '@angular/core';
+
+/**
+ * Examples:
+ * MockComponent({ selector: 'cranium' });
+ * MockComponent({ selector: 'arm', inputs: ['side'] });
+ */
+export function MockComponent(selector: string): Component {
+  const metadata: Component = {
+    selector: selector,
+    template: null,
+    inputs: null,
+    outputs: null
+  };
+  return new Component(metadata);
+}
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     }).compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to webapp!');
-  }));
+
+  it('should have a message component', () => {
+    const messageDirective = fixture.debugElement.query(By.css('app-message'));
+
+    expect(messageDirective).toBeTruthy();
+  });
+
+  it('should have a login component', () => {
+    const loginDirective = fixture.debugElement.query(By.css('app-login'));
+
+    expect(loginDirective).toBeTruthy();
+  });
 });
