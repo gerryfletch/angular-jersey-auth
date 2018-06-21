8b58ed8ad18cef432a286295635cd854b96b503d
gerryfletch <1997gerry@gmail.com>
2018-06-21 18:48:58 +0100

    Wrote unit tests for getting public and private messages.

--- webapp/src/app/_services/message.service.spec.ts
+++ webapp/src/app/_services/message.service.spec.ts
@@ -1,15 +1,64 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {getTestBed, TestBed} from '@angular/core/testing';
 
 import {MessageService} from './message.service';
+import {AuthenticationService} from './authentication.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
-fdescribe('MessageService', () => {
+describe('MessageService', () => {
+
+  let injector: TestBed;
+  let service: MessageService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [MessageService]
+    });
+    injector = getTestBed();
+    service = injector.get(MessageService);
+    httpMock = injector.get(HttpTestingController);
+  });
+
+  // Make sure that there are no outstanding requests
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  describe('When not logged in', () => {
+    const message = 'public';
+
+    beforeEach(() => {
+      spyOn(injector.get(AuthenticationService), 'isLoggedIn').and.returnValue(false);
+    });
+
+    it('should return a public message', () => {
+      service.getMessage().subscribe(data => {
+        expect(data.message).toBe(message);
+      });
+
+      const req = httpMock.expectOne('/api/message/public');
+      expect(req.request.method).toBe('GET');
+      req.flush({'message': message});
     });
   });
 
-  it('should be created', inject([MessageService], (service: MessageService) => {
-    expect(service).toBeTruthy();
-  }));
+  describe('When logged in', () => {
+    const message = 'private';
+
+    beforeEach(() => {
+      spyOn(injector.get(AuthenticationService), 'isLoggedIn').and.returnValue(true);
+    });
+
+    it('should return a private message', () => {
+      service.getMessage().subscribe(data => {
+        expect(data.message).toBe(message);
+      });
+
+      const req = httpMock.expectOne('/api/message/private');
+      expect(req.request.method).toBe('GET');
+      req.flush({'message': message});
+    });
+  });
+
 });
