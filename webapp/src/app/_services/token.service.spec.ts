e6b104a9edec7b4f5df72bfe6721f778f43d7b89
gerryfletch <1997gerry@gmail.com>
2018-06-21 22:27:44 +0100

    Unit tested all local storage operations and the keys they use.

--- webapp/src/app/_services/token.service.spec.ts
+++ webapp/src/app/_services/token.service.spec.ts
@@ -1,30 +1,84 @@
 import {TokenService} from './token.service';
+import {Tokens} from '../_models/tokens.model';
 import Spy = jasmine.Spy;
 
+/**
+ * I'm not sure of the best way to test the local storage getting and setting.
+ */
 fdescribe('TokenService', () => {
 
   let localStorageGetSpy: Spy;
-  let localStoragePutSpy: Spy;
+  let localStorageSetSpy: Spy;
   let service: TokenService;
+
+  const mockTokens = {
+    refresh_token: 'xxx.yyy.zzz',
+    access_token: 'aaa.bbb.ccc'
+  } as Tokens;
+
+  const REFRESH_KEY = 'refresh_token';
+  const ACCESS_KEY = 'access_token';
 
   beforeEach(() => {
     service = new TokenService();
     localStorageGetSpy = spyOn(localStorage, 'getItem');
-    localStoragePutSpy = spyOn(localStorage, 'setItem');
+    localStorageSetSpy = spyOn(localStorage, 'setItem');
   });
 
-  describe('IsLoggedIn', () => {
-    it('returns not logged in if no tokens exist', () => {
+  // Clear the local storage
+  afterEach(() => {
+    localStorage.clear();
+  });
+
+  describe('Checking if the user is logged in', () => {
+    it('returns false if no tokens exist', () => {
       localStorageGetSpy.and.returnValue(null);
 
-      expect (service.isLoggedIn()).toBeFalsy();
+      const isLoggedIn = service.isLoggedIn();
+
+      expect(isLoggedIn).toBeFalsy();
+      expect(localStorageGetSpy).toHaveBeenCalledWith(REFRESH_KEY);
     });
 
-    it('returns logged in if tokens exist', () => {
-      localStorageGetSpy.and.returnValue({});
+    it('returns true if tokens exist', () => {
+      localStorageGetSpy.and.returnValue(mockTokens.refresh_token);
 
-      expect(service.isLoggedIn()).toBeTruthy();
+      const isLoggedIn = service.isLoggedIn();
+
+      expect(isLoggedIn).toBeTruthy();
+      expect(localStorageGetSpy).toHaveBeenCalledWith(REFRESH_KEY);
     });
+  });
+
+  describe('Getting tokens', () => {
+
+    it('should return a refresh token', () => {
+      localStorageGetSpy.and.returnValue(mockTokens.refresh_token);
+
+      const refreshToken = service.getRefreshToken();
+
+      expect(localStorageGetSpy).toHaveBeenCalledWith(REFRESH_KEY);
+      expect(refreshToken).toBe(mockTokens.refresh_token);
+    });
+
+
+    it('should return an access token', () => {
+      localStorageGetSpy.and.returnValue(mockTokens.access_token);
+
+      const accessToken = service.getAccessToken();
+
+      expect(accessToken).toBe(mockTokens.access_token);
+    });
+  });
+
+  describe('Setting tokens', () => {
+    it('should call set item for both keys', () => {
+      service.setTokens(mockTokens);
+
+      expect(localStorageSetSpy).toHaveBeenCalledWith(REFRESH_KEY, mockTokens.refresh_token);
+      expect(localStorageSetSpy).toHaveBeenCalledWith(ACCESS_KEY, mockTokens.access_token);
+    });
+
   });
 
 });
