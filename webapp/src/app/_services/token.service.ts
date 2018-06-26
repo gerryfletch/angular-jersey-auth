173eba825d84de83ff999a688cf3fe5a99243e25
gerryfletch <1997gerry@gmail.com>
2018-06-26 13:54:44 +0100

    Created functions to store access and refresh tokens specifically.

--- webapp/src/app/_services/token.service.ts
+++ webapp/src/app/_services/token.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Tokens} from '../_models/tokens.model';
+import {Token} from '../_models/token.model';
 
 @Injectable({
   providedIn: 'root'
@@ -24,7 +25,15 @@ export class TokenService {
     return localStorage.getItem(this.ACCESS_KEY);
   }
 
-  setTokens(token: Tokens) {
+  setAccessToken(token: Token): void {
+    localStorage.setItem(this.ACCESS_KEY, token.token);
+  }
+
+  setRefreshToken(token: Token): void {
+    localStorage.setItem(this.REFRESH_KEY, token.token);
+  }
+
+  setTokens(token: Tokens): void {
     localStorage.setItem(this.REFRESH_KEY, token.refresh_token);
     localStorage.setItem(this.ACCESS_KEY, token.access_token);
   }
