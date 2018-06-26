c772f844499d67efca3a1161f85092b8dc29f06c
gerryfletch <1997gerry@gmail.com>
2018-06-26 14:12:34 +0100

    Changed authentication service to use access only models and functions.

--- webapp/src/app/_services/authentication.service.ts
+++ webapp/src/app/_services/authentication.service.ts
@@ -3,14 +3,15 @@ import {HttpClient, HttpErrorResponse, HttpHeaders, HttpParams} from '@angular/c
 import {Observable, throwError} from 'rxjs/index';
 import {Tokens} from '../_models/tokens.model';
 import {TokenService} from './token.service';
-import {catchError, tap} from 'rxjs/internal/operators';
+import {catchError, map, tap} from 'rxjs/internal/operators';
+import {AccessToken, Token} from '../_models/access-token.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  private authReqHandler: Observable<Tokens>;
+  private authReqHandler: Observable<Token>;
 
   constructor(private http: HttpClient, private tokenService: TokenService) {
     this.authReqHandler = null;
@@ -52,15 +53,15 @@ export class AuthenticationService {
   }
 
   /**
-   * Makes a white-listed request to the refresh end point with the refresh token.
-   * A new refresh-access token pair is returned, or an error is thrown.
+   * Makes a white-listed request to the refresh end point with the refresh token, expecting a valid access token in return.
+   * If a 401 unauthorized error is thrown, the user is no longer authenticated.
    *
    * To prevent multiple concurrent requests to refresh tokens, we store an observable
    * of tokens, 'authReqHandler'. The initial http request is assigned to this observable,
    * and it is returned to any subsequent requests.
-   * @returns {Observable<Tokens>}  A new valid refresh-access token pair.
+   * @returns {Observable<Token>}  A new valid refresh-access token pair.
    */
-  refreshTokens(): Observable<Tokens> {
+  refreshAccessToken(): Observable<Token> {
     if (!! this.authReqHandler) {
       return this.authReqHandler;
     }
@@ -73,12 +74,9 @@ export class AuthenticationService {
       })
     };
 
-    this.authReqHandler = this.http.get<Tokens>('/api/auth/refresh', httpOptions)
-      .pipe(
-        tap((tokens: Tokens) => {
-          this.tokenService.setTokens(tokens);
-        })
-      );
+    this.authReqHandler = this.http.get<AccessToken>('/api/auth/refresh', httpOptions).pipe(
+        tap((token: AccessToken) => this.tokenService.setAccessToken(token.access_token))
+    );
 
     return this.authReqHandler;
   }
