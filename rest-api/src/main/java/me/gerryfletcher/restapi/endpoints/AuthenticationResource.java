a9fef099deea8a350005a609070f9a75d40ea01d
gerryfletch <1997gerry@gmail.com>
2018-06-27 23:59:08 +0100

    Changed security context to container and got rid of exceptions.

--- rest-api/src/main/java/me/gerryfletcher/restapi/endpoints/AuthenticationResource.java
+++ rest-api/src/main/java/me/gerryfletcher/restapi/endpoints/AuthenticationResource.java
@@ -1,14 +1,11 @@
 package me.gerryfletcher.restapi.endpoints;
 
-import com.auth0.jwt.exceptions.JWTCreationException;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import me.gerryfletcher.restapi.authentication.AuthenticationService;
 import me.gerryfletcher.restapi.authentication.Role;
 import me.gerryfletcher.restapi.authentication.UserSecurityContext;
 import me.gerryfletcher.restapi.exceptions.InvalidLoginException;
-import me.gerryfletcher.restapi.exceptions.permissions.UserLoggedOutException;
-import me.gerryfletcher.restapi.exceptions.permissions.UserRevokedException;
 import me.gerryfletcher.restapi.models.AuthTokens;
 import me.gerryfletcher.restapi.models.User;
 import me.gerryfletcher.restapi.permissions.PermissionAction;
@@ -19,10 +16,10 @@ import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
+import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
 import java.util.Date;
 
 @Path("auth")
@@ -46,7 +43,7 @@ public class AuthenticationResource {
     @Path("login")
     @PermitAll
     @Produces(MediaType.APPLICATION_JSON)
-    public Response login(@NotNull @FormParam("username") String username, @NotNull @FormParam("password") String password) throws InvalidLoginException, JWTCreationException {
+    public Response login(@NotNull @FormParam("username") String username, @NotNull @FormParam("password") String password) {
         AuthTokens authTokens = authService.login(username, password);
         return Response.ok().entity(gson.toJson(formResponse(authTokens))).build();
     }
@@ -60,8 +57,8 @@ public class AuthenticationResource {
     @Path("refresh")
     @RolesAllowed(Role.REFRESH)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response refreshAccessToken(@Context SecurityContext cont) throws JWTCreationException, UserRevokedException, UserLoggedOutException {
-        UserSecurityContext context = (UserSecurityContext) cont;
+    public Response refreshAccessToken(@Context ContainerRequestContext cont) {
+        UserSecurityContext context = (UserSecurityContext) cont.getSecurityContext();
         User user = context.getUserPrincipal();
 
         String accessToken = authService.refreshAccessToken(user.getName(), user.getRole(), context.getToken());
@@ -70,7 +67,7 @@ public class AuthenticationResource {
         return Response.ok().entity(accessTokenResponse).build();
     }
 
-    private JsonObject formResponse(AuthTokens authTokens) throws JWTCreationException {
+    private JsonObject formResponse(AuthTokens authTokens) {
         JsonObject result = new JsonObject();
         result.addProperty("access_token", authTokens.getAccessToken());
         result.addProperty("refresh_token", authTokens.getRefreshToken());
