a8a0fd125c2a9e229ad6cea57862e956951d2b0a
gerryfletch <1997gerry@gmail.com>
2018-06-27 15:26:30 +0100

    Created basic permission change roles.

---
+++ rest-api/src/main/java/me/gerryfletcher/restapi/data/PermissionAction.java
@@ -0,0 +1,12 @@
+package me.gerryfletcher.restapi.data;
+
+public enum PermissionAction {
+    // Make a user -> admin
+    PROMOTE,
+    // Make a admin -> user
+    DEMOTE,
+    // Clear a user of a revokation
+    CLEAR,
+    // Revoke a users account
+    REVOKE
+}
---
+++ rest-api/src/main/java/me/gerryfletcher/restapi/data/UserPermissions.java
@@ -0,0 +1,39 @@
+package me.gerryfletcher.restapi.data;
+
+import java.util.Date;
+
+public class UserPermissions {
+
+    private String username;
+    private PermissionAction action;
+    private Date timestamp;
+
+    private String description;
+
+    public UserPermissions(String username, PermissionAction action, Date timestamp) {
+        this.username = username;
+        this.action = action;
+        this.timestamp = timestamp;
+    }
+
+    public UserPermissions(String username, PermissionAction action, Date timestamp, String description) {
+        this(username, action, timestamp);
+        this.description = description;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public PermissionAction getAction() {
+        return action;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getDescription() {
+        return (description != null) ? description : "";
+    }
+}
