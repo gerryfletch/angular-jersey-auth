1db95f793ee0c8bac7224aef7c047af35f23ed36
gerryfletch <1997gerry@gmail.com>
2018-06-27 15:07:32 +0100

    Documented roles.

--- rest-api/src/main/java/me/gerryfletcher/restapi/authentication/Role.java
+++ rest-api/src/main/java/me/gerryfletcher/restapi/authentication/Role.java
@@ -1,5 +1,8 @@
 package me.gerryfletcher.restapi.authentication;
 
+/**
+ * The roles available to the end points.
+ */
 public class Role {
     public static final String ADMIN = "ADMIN";
     public static final String USER = "USER";
