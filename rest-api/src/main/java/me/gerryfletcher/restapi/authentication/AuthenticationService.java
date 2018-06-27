d79f655173eede830b715916c0d8b10fe4e355cd
gerryfletch <1997gerry@gmail.com>
2018-06-27 16:36:42 +0100

    Got rid of possibility of no token, and did a permission check for user revokation.

--- rest-api/src/main/java/me/gerryfletcher/restapi/authentication/AuthenticationService.java
+++ rest-api/src/main/java/me/gerryfletcher/restapi/authentication/AuthenticationService.java
@@ -2,11 +2,14 @@ package me.gerryfletcher.restapi.authentication;
 
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import me.gerryfletcher.restapi.exceptions.AuthenticationException;
 import me.gerryfletcher.restapi.exceptions.InvalidLoginException;
+import me.gerryfletcher.restapi.exceptions.UserRevokedException;
 import me.gerryfletcher.restapi.models.AuthTokens;
+import me.gerryfletcher.restapi.permissions.PermissionAction;
 import me.gerryfletcher.restapi.permissions.PermissionService;
 import me.gerryfletcher.restapi.permissions.UserPermissions;
+
+import java.util.Date;
 
 public class AuthenticationService {
 
@@ -22,6 +25,7 @@ public class AuthenticationService {
      * Check if the user has the correct credentials.
      * This would be done using a database and hashed + salted passwords.
      * <b>This is purely an example.</b>
+     *
      * @return The newly created Auth Tokens.
      * @throws InvalidLoginException The username and password combination is incorrect.
      */
@@ -37,11 +41,24 @@ public class AuthenticationService {
 
     /**
      * Checks if a user is permitted to refresh a token.
+     *
      * @return A new access token with the users details.
      */
-    public String refreshAccessToken(String username, String role, DecodedJWT token) throws JWTCreationException, AuthenticationException {
-        if (token == null) throw new AuthenticationException();
+    public String refreshAccessToken(String username, String role, DecodedJWT token) throws JWTCreationException, UserRevokedException {
+        Date tokenDate = token.getIssuedAt();
+
         UserPermissions userPermissions = permissionService.getUserPermissions(username);
+        Date permissionDate = userPermissions.issuedAt();
+
+        // The permissions have been updated since the token was issued
+        if (tokenDate.before(permissionDate)) {
+            PermissionAction action = userPermissions.getAction();
+            switch (action) {
+                case REVOKE:
+                    throw new UserRevokedException();
+            }
+        }
+
         return tokenService.createAccessToken(username, role);
     }
 
