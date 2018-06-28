73c8e48af552b8177247f1e998b7ca9c386e7b46
gerryfletch <1997gerry@gmail.com>
2018-06-28 20:27:54 +0100

    Changed Auth Filter to use new roles and simplified it all.

---
+++ rest-api/src/main/java/me/gerryfletcher/restapi/permissions/UserInfo.java
@@ -0,0 +1,27 @@
+package me.gerryfletcher.restapi.permissions;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+import me.gerryfletcher.restapi.authentication.Role;
+
+public class UserInfo {
+    private String username;
+    private boolean loggedIn = true;
+    private boolean revoked = false;
+    Role role = Role.USER;
+
+    public UserInfo fromAccessToken(DecodedJWT token) {
+        this.username = token.getClaim("username").asString();
+        return this;
+    }
+
+    public UserInfo setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+        return this;
+    }
+
+    public UserInfo setRole(Role role) {
+        this.role = role;
+        return this;
+    }
+
+}
